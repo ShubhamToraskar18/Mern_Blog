9f7336d8eacfd199ada1351503791a44bdf2ffe5
ShubhamToraskar18 <shubhamtoraskar8g@gmail.com>
2025-08-06 10:08:32 +0530

    Models Done

---
+++ backend/src/models/post.model.js
@@ -0,0 +1,31 @@
+import mongoose, { Schema } from "mongoose";
+
+const postSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true  
+    },
+    userId: {
+        type: mongoose.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    postImage: {
+        type: String,
+        default: "https://cdn.wallpapersafari.com/51/76/3TFfqI.jpg"
+    },
+    slug: {
+        type: String,
+        required: true,
+        unique: true
+    }
+
+}, {timestamps: true})
+
+const Post = mongoose.model('Post', postSchema);
+
+export default Post;
